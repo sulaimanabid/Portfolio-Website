93b4ce51016001f94a008dfc000b611683545404
sulaimanabid <sulaimanabid3@gmail.com>
2024-10-17 13:47:07 +0500

    experience updated

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -14,15 +14,23 @@ const Experience: React.FC<{}> = () => {
               Full-Stack Developer
             </p>
           </div>
-          <p className="text-gray-300 md:pt-0 pt-3">April 2024 - Present</p>
+          <p className="text-gray-300 md:pt-0 pt-3">Dec 2023 - Present</p>
         </div>
-        <p className="text-gray-300 pt-5" style={{ textAlign: 'justify' }}>
+        <p className="text-gray-300 pt-5" style={{ textAlign: "justify" }}>
           Engaged in the development of a dental application, transitioning the
           app from Angular 1 to React JS. Utilizing react2angular and
           angular2react libraries for component migration. Responsible for
           upkeeping the current backend APIs using Django and PostgreSQL.
           Collaborating with AWS to manage the continuous integration and
           deployment (CI/CD) processes. Using JIRA for task management.
+        </p>
+
+        <p className="text-gray-300 pt-5" style={{ textAlign: "justify" }}>
+          Worked on Decentralized Apps (DApps), including Piqsol, a multichain
+          real-time NFT marketplace built on Next.js 14. Used MUI and
+          styled-components for design. Handled Trust Wallet and Metamask
+          integration using the web3-react library, and managed smart contract
+          integration across Polygon, Ethereum, AVAX, and Binance chains.
         </p>
 
         <div className="flex-col flex sm:flex-row flex-wrap gap-2">
@@ -46,26 +54,6 @@ const Experience: React.FC<{}> = () => {
           <div className="bg-transparent mt-5 mr-2 cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] w-max">
             Postgres
           </div>
-        </div>
-        <br></br>
-        <div className="md:flex md:flex-row md:justify-between pt-5">
-          <div className="flex items-center gap-3">
-            <p className="text-gray-300">
-              <span className="font-semibold">BLOCKMOB LABS /</span> Front-End
-              Web3 Developer
-            </p>
-          </div>
-          <p className="text-gray-300 md:pt-0 pt-3">Dec 2023 - Apr 2024</p>
-        </div>
-        <p className="text-gray-300 pt-5" style={{ textAlign: 'justify' }}>
-          Worked on Decentralized Apps (DApps), including Piqsol, a multichain
-          real-time NFT marketplace built on Next.js 14. Used MUI and
-          styled-components for design. Handled Trust Wallet and Metamask
-          integration using the web3-react library, and managed smart contract
-          integration across Polygon, Ethereum, AVAX, and Binance chains.
-        </p>
-
-        <div className="flex-col flex sm:flex-row flex-wrap gap-2">
           <div className="bg-transparent mt-5 mr-2 cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] w-max">
             Next.js
           </div>
@@ -87,6 +75,7 @@ const Experience: React.FC<{}> = () => {
             Ethereum
           </div>
         </div>
+
         <br></br>
 
         <div className="md:flex md:flex-row md:justify-between pt-5">
@@ -98,7 +87,7 @@ const Experience: React.FC<{}> = () => {
           </div>
           <p className="text-gray-300 md:pt-0 pt-3">Apr 2023 - Dec 2023</p>
         </div>
-        <p className="text-gray-300 pt-5" style={{ textAlign: 'justify' }}>
+        <p className="text-gray-300 pt-5" style={{ textAlign: "justify" }}>
           Worked on the main WW HUB, handling frontend components using React.
           Developed PMD Hub for Pakistan Meteorological Department using React,
           React Query for API handling, and Ant Design for building UI
@@ -137,9 +126,9 @@ const Experience: React.FC<{}> = () => {
               Front-End React Developer
             </p>
           </div>
-          <p className="text-gray-300 md:pt-0 pt-3">Jul 2022 - Apr 2023</p>
+          <p className="text-gray-300 md:pt-0 pt-3">Dec 2021 - Apr 2023</p>
         </div>
-        <p className="text-gray-300 pt-5" style={{ textAlign: 'justify' }}>
+        <p className="text-gray-300 pt-5" style={{ textAlign: "justify" }}>
           Worked on the AQG NOW and Vehicle Wallet projects, handling frontend
           logic and API fetching using React. Managed frontend components with
           Reactstrap and ensured responsiveness. Integrated Google Maps to show
@@ -163,32 +152,6 @@ const Experience: React.FC<{}> = () => {
           </div>
           <div className="bg-transparent mt-5 mr-2 cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] w-max">
             React Google Maps
-          </div>
-        </div>
-        <br></br>
-
-        <div className="md:flex md:flex-row md:justify-between pt-5">
-          <div className="flex items-center gap-3">
-            <p className="text-gray-300">
-              <span className="font-semibold">DAXNO TECHNOLOGIES /</span> Junior
-              Front-End React Developer
-            </p>
-          </div>
-          <p className="text-gray-300 md:pt-0 pt-3">Dec 2021 - Apr 2022</p>
-        </div>
-        <p className="text-gray-300 pt-5" style={{ textAlign: 'justify' }}>
-          Worked on various marketplace and e-commerce websites, creating
-          responsive designs and polishing JavaScript skills. Followed a
-          component-based approach using React Hooks to build faster web
-          applications.
-        </p>
-
-        <div className="flex-col flex sm:flex-row flex-wrap gap-2">
-          <div className="bg-transparent mt-5 mr-2 cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] w-max">
-            React.js
-          </div>
-          <div className="bg-transparent mt-5 mr-2 cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] w-max">
-            JavaScript
           </div>
         </div>
       </div>
